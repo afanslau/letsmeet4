5fcdd46ff9ace319f845ad684de897a49997ee7e
eshim <eshim@eshim-Sat.(none)>
2014-03-30 16:00:25 -0400

    frames added

--- letsmeettemplate/www/js/custom.js
+++ letsmeettemplate/www/js/custom.js
@@ -1,7 +1,7 @@
 // Responsive Calendar
 $( document ).ready( function() {
   $('.responsive-calendar').responsiveCalendar({
-  	// onDayClick: function(events) { alert('You have made a meeting') },
+  	onDayClick: function(events) { alert('You have sent a message!') },
     events: {
       "2014-03-29": {"number": 5, "badgeClass": "badge-warning", "url": "#"},
       "2014-03-30": {"number": 1, "badgeClass": "badge-warning", "url": "#"}, 
